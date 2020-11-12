1edd69f5ee77f906e7049e6749e70134d2ca116e
Broch <72731657+Brochy98@users.noreply.github.com>
2020-11-12 12:38:22 +0100

    update
    added context to pages

--- Portfolie/javascript.js
+++ Portfolie/javascript.js
@@ -1,1 +1,16 @@
+function UpdateTitle(){
+    var titles = ["Aspiring Game Developer", "Dungeon-Master", "Map maker", "Programmer", "Dane", "Gamer", "Student"];
+
+    if(typeof UpdateTitle.counter == 'undefined')
+    {
+        UpdateTitle.counter = 0;
+    }
+
+    document.getElementById("undertitle").innerText = titles[UpdateTitle.counter %titles.length];
+
+    UpdateTitle.counter++;
+
+    setTimeout(UpdateTitle, 2000);
+}
+
 
